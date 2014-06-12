07b6c22bf92dbcc37f5651b9b27d101dc6dd984e
gustavo82rs <guslarsen@hotmail.com>
2014-06-12 20:14:04 -0300

    CreditController
    Criada Action Buy

--- uniTunes/uniTunes.UI/Controllers/CreditController.cs
+++ uniTunes/uniTunes.UI/Controllers/CreditController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using uniTunes.Services;
 
 namespace uniTunes.UI.Controllers
 {
     public class CreditController : Controller
     {
+        private readonly ICreditService CreditService;
+                
         // GET: Credit
         public ActionResult Index()
         {
